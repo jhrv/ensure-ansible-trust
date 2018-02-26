aecefe4b3ec654a6e8249344d3b17592275af780
Johnny Horvi <jhrv@users.noreply.github.com>
2018-02-26 12:27:58 +0100

    split help into function for readability

--- ensure-ansible-trust.sh
+++ ensure-ansible-trust.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 function ensure-ansible-trust {
-	if [[ "${#}" -eq 0 || "${1}" == "-h" || "${1}" == "--help" ]]; then
-        cat <<EOF 
+    if [[ "${#}" -eq 0 || "${1}" == "-h" || "${1}" == "--help" ]]; then
+        print_halp
+	return
+    fi
+
+    INVENTORY=${1}
+    USER=${2}
+    
+    for HOST in $(ansible all -i ${INVENTORY} --list-hosts | tail -n+2 | tr -d " "); do
+    	ssh-copy-id ${USER}@${HOST}
+    done
+}
+
+function print_halp {
+    cat <<EOF 
 ensure-ansible-trust
 
 Usage: ensure-ansible-trust [-h | --help] inventory-file username 
@@ -12,13 +25,4 @@ Ensure ssh-trust with all hosts in provided inventory file with username
   -h --help    print this message
 
 EOF
-		return
-    fi
-
-    INVENTORY=${1}
-    USER=${2}
-    
-    for HOST in $(ansible all -i ${INVENTORY} --list-hosts | tail -n+2 | tr -d " "); do
-    	ssh-copy-id ${USER}@${HOST}
-    done
 }
