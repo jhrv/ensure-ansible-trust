c26d377d50ad46aafbc3d2c7a917db423e1f7d0e
Johnny Horvi <johnny.horvi@nav.no>
2018-02-26 12:11:29 +0100

    added readme and made functioooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooon

---
+++ README.md
@@ -0,0 +1,21 @@
+# ensure-ansible-trust
+
+```
+ensure-ansible-trust
+
+Usage: ensure-ansible-trust [-h | --help] inventory-file username 
+
+Ensure ssh-trust with all hosts in provided inventory file with username
+
+  -h --help    print this message
+
+```
+
+## usage
+
+```
+$ git clone https://github.com/jhrv/ensure-ansible-trust
+$ cd ensure-ansible-trust
+$ source ensure-ansible-trust-sh
+$ ensure-ansible-trust --help
+```
--- ensure-ansible-trust.sh
+++ ensure-ansible-trust.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-INVENTORY=${1}
-USER=${2}
+function ensure-ansible-trust {
+	if [[ "${#}" -eq 0 || "${1}" == "-h" || "${1}" == "--help" ]]; then
+        cat <<EOF 
+ensure-ansible-trust
 
-for HOST in $(ansible all -i ${INVENTORY} --list-hosts | tail -n+2 | tr -d " "); do
-	ssh-copy-id ${USER}@${HOST}
-done
+Usage: ensure-ansible-trust [-h | --help] inventory-file username 
+
+Ensure ssh-trust with all hosts in provided inventory file with username
+
+  -h --help    print this message
+
+EOF
+		return
+    fi
+
+    INVENTORY=${1}
+    USER=${2}
+    
+    for HOST in $(ansible all -i ${INVENTORY} --list-hosts | tail -n+2 | tr -d " "); do
+    	ssh-copy-id ${USER}@${HOST}
+    done
+}
